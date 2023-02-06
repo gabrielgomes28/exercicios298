d34a0ce5161790da8b0d2fe95d8ac59fce6d9a24
gabriel <bielgomes123>
2023-02-06 12:17:28 -0300

    commit inicial

---
+++ Nova pasta/src/funcoes.ts
@@ -0,0 +1,8 @@
+function multiplica (numero1:number=3 , numero2:number=9) :number{
+    return numero1 * numero2; 
+    console.log(multiplica)
+}
+
+function dizOI(nome:string="gabriel"){
+    return '${this.nome} disse oi';
+}
